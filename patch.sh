37724bd937124c54a771eee6b36dbfe395ccf267
Yijun Yu <y.yu@open.ac.uk>
2018-12-30 22:28:35 +0000

    remove the submodules

--- patch.sh
+++ patch.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-if [ ! -d gen.antlr3-smali ]; then
-	git clone https://github.com/JesusFreke/smali gen.antlr3-smali
-	cp -r smali.patch/* gen.antlr3-smali
-	cd !$
-	gradle build -x test -x checkstyleMain
-fi
+#if [ ! -d gen.antlr3-smali ]; then
+#	git clone https://github.com/JesusFreke/smali gen.antlr3-smali
+#	cp -r smali.patch/* gen.antlr3-smali
+#	cd !$
+#	gradle build -x test -x checkstyleMain
+#fi
 gradle build -x test -x checkstyleMain
 gumtree=gumtree-$(date '+%Y%m%d')-2.1.0-SNAPSHOT
 unzip -o dist/build/distributions/$gumtree.zip
